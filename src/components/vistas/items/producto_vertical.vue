7ec65fce6056986a9e3b8a7894230229eb11d490
lahrweb <70175030+lahrweb@users.noreply.github.com>
2023-07-14 13:46:06 -0400

    Inicial

---
+++ src/components/vistas/items/producto_vertical.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(()=>{
+    
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
